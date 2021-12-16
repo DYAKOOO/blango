42b65e23104690c82c017b913aca377656220dd1
Diako Rudd <diako.rudd@outlook.com>
2021-12-16 06:07:38 +0000

    finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -41,8 +41,17 @@ def row(extra_classes=""):
 def endrow():
     return format_html("</div>")
 
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">',extra_classes)
+  
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
     return {"title": "Recent Posts", "posts": posts}
- 
+
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.views import generic
 from blog.models import Post
-
+from django.shortcuts import redirect
+#from django.shortcuts import render
+from blog.forms import CommentForm
 
 # Create your views here.
 def index(request):
@@ -11,4 +13,21 @@ def index(request):
 
 def post_detail(request, slug=None):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {"post": post})
+    if request.user.is_active:
+      if request.method == "POST":
+          comment_form = CommentForm(request.POST)
+          if comment_form.is_valid():
+              comment = comment_form.save(commit=False)
+              comment.content_object = post
+              comment.creator = request.user
+              comment.save()
+              return redirect(request.path_info)
+      else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+        
+    return render(
+        request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
+    )    
+    
