ab798698767edd4e82474c75703c0959595753d5
Diako Rudd <diako.rudd@outlook.com>
2022-01-12 04:33:11 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,8 +4,11 @@ from blog.api.serializers import PostSerializer
 from blog.models import Post
 from blog.api.permissions import AuthorModifyOrReadOnly
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 
 class PostList(generics.ListCreateAPIView):
+    lookup_field = "email"
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
@@ -14,3 +17,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
