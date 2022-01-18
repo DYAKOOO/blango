bb6ac2267dcbece40b4c3e5651b82962581168da
Diako Rudd <diako.rudd@outlook.com>
2022-01-18 02:05:17 +0000

    Finish Viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -56,3 +56,9 @@ class PostDetailSerializer(PostSerializer):
             comment.save()
 
         return instance
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList,PostDetail
-from blog.api.views import UserDetail
+from blog.api.views import UserDetail, TagViewSet , PostViewSet
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
 
-
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,8 +22,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    # path("posts/", PostList.as_view(), name="api_post_list"),
+    # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
@@ -35,6 +37,7 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
