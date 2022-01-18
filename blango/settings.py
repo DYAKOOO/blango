33c1cf86ae4a1cbbebbb6dc15eea4d7240f3c60f
Diako Rudd <diako.rudd@outlook.com>
2022-01-18 00:55:11 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -70,13 +70,20 @@ class Dev(Configuration):
       "allauth.socialaccount.providers.google",
       "rest_framework",
       "rest_framework.authtoken",
+      "drf_yasg",
   ]
+  SWAGGER_SETTINGS = {
+      "SECURITY_DEFINITIONS": {
+          "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+          "Basic": {"type": "basic"},
+      }
+  }
+
   REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
