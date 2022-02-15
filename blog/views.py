10fbc317837bf2a0c4ab31a9aba64a5938a36f20
Diako Rudd <diako.rudd@outlook.com>
2022-02-15 23:31:54 +0000

    Finish Javasript Intro and Fundamentals

--- blog/views.py
+++ blog/views.py
@@ -44,4 +44,8 @@ def post_detail(request, slug=None):
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
+
   
